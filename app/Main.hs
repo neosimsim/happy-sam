6cc07106890ddf667c6a02a9445c65b8fc293cfd
Alexander Ben Nasrallah <me@abn.sh>
2019-02-20 18:18:22 +0100

    parse each command of the input

--- app/Main.hs
+++ app/Main.hs
@@ -5,4 +5,14 @@ module Main
 import           Sheila.Parser
 
 main :: IO ()
-main = getContents >>= print . parseFoo
+main = getContents >>= parseAndPrint
+
+parseAndPrint :: String -> IO ()
+parseAndPrint s =
+  case parseCommand s of
+    Left e -> putStrLn e
+    Right (cmd, rest) -> do
+      print cmd
+      case rest of
+        [] -> return ()
+        _  -> parseAndPrint rest
